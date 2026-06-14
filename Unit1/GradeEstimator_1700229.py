b11876e663fbabb802ef1c24fc684fe209680586
Dilip24-00 <1700229@park.edu>
2026-06-14 12:24:24 -0500

    Add files via upload

---
+++ Unit1/GradeEstimator_1700229.py
@@ -0,0 +1,9 @@
+
+# Ask the user for their first name so we can store it and use it later in the program
+first_name = input("Enter your first name: ")
+
+# Ask the user for their last name so we can combine it with first name for full name output
+last_name = input("Enter your last name: ")
+
+# Print the full name so the user can see their first and last name together in proper format
+print(first_name + " " + last_name)
